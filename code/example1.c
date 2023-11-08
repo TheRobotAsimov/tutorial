0264bf9883fed780be957b1cafc7b65d4fc67083
Robot Asimov <th3r0b0t4s1m0v@gmail.com>
2023-11-07 21:19:49 -0600

    Add c2 variable to example1.c

--- code/example1.c
+++ code/example1.c
@@ -5,11 +5,13 @@ int main(void)
     int a = 10;
     int b = 20;
     int c = 30;
+    int c2 = 32;
     int d = 40;
 
     printf("a = %d\n", a);
     printf("b = %d\n", b);
     printf("c = %d\n", c);
+    printf("c2 = %d\n", c2);
     printf("d = %d\n", d);
 
     return 0;
